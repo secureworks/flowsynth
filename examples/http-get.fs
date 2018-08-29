0d27ce48431bfed2e5107488e10f7e52912a0ce2
mithrilwoodrat <mithrilwoodrat@gmail.com>
2018-08-29 23:37:52 +0800

    fix Content-Length,so wireshark could recognize generated pcap file

--- examples/http-get.fs
+++ examples/http-get.fs
@@ -1,3 +1,3 @@
 flow default tcp example.com:44123 > google.com:80 (tcp.initialize;);
 default > (content:"GET / HTTP/1.1\x0d\x0aHost:google.com\x0d\x0aUser-Agent: DogBot\x0d\x0a\x0d\x0a";);
-default < (content:"HTTP/1.1 200 OK\x0d\x0aContent-Length: 300\x0d\x0a\x0d\x0aWelcome to Google.com!\x0d\x0a\x0d\x0a";);
+default < (content:"HTTP/1.1 200 OK\x0d\x0aContent-Length: 26\x0d\x0a\x0d\x0aWelcome to Google.com!\x0d\x0a\x0d\x0a";);
